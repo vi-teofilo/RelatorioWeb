41ebac6f8dcfbde42952eec7f5a9059a29539b4f
Vitória Teófilo Furtado <vfurtado@server01.foursys.local>
2020-11-27 17:34:44 -0300

    Relatorio

---
+++ src/br/com/foursys/relatorio/controller/ClienteController.java
@@ -0,0 +1,73 @@
+package br.com.foursys.relatorio.controller;
+
+import java.util.ArrayList;
+
+import br.com.foursys.relatorio.bean.Cliente;
+import br.com.foursys.relatorio.dao.ClienteDAO;
+import br.com.foursys.relatorio.vo.ClienteVO;
+
+public class ClienteController {
+
+	public void salvar(Cliente cliente) {
+		ClienteDAO dao = new ClienteDAO();
+		try {
+			dao.salvar(cliente);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void excluir(Cliente cliente) {
+		ClienteDAO dao = new ClienteDAO();
+		try {
+			dao.excluir(cliente);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<Cliente> buscarTodos(){
+		ArrayList<Cliente> listaRetorno = new ArrayList<Cliente>();
+		ClienteDAO dao = new ClienteDAO();
+		try {
+			listaRetorno = dao.buscarTodos();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return listaRetorno;
+	}
+	
+	public ArrayList<Cliente> buscarNome(String nome){
+		ArrayList<Cliente> listaRetorno = new ArrayList<Cliente>();
+		ClienteDAO dao = new ClienteDAO();
+		try {
+			listaRetorno = dao.buscarNome(nome);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return listaRetorno;
+	}
+	
+	public ArrayList<ClienteVO> retornaClientes(){
+		ArrayList<ClienteVO> listaRetorno = new ArrayList<ClienteVO>();
+		try {
+			ArrayList<Cliente> listaClientes = new ClienteDAO().buscarTodos();
+			for (Cliente cliente : listaClientes) {
+				ClienteVO vo = new ClienteVO();
+				vo.setNome(cliente.getNome());
+				vo.setTelefone(cliente.getTelefone());
+				vo.setDataNascimento(cliente.getDataNascimento());
+				vo.setCidade(cliente.getCidade());
+				listaRetorno.add(vo);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+		return listaRetorno;
+		
+	}
+
+}
